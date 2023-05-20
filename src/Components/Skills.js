ea52fe1aa9e9c58a3a0c3a188d9e1ff59e724594
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-05-20 18:21:59 -0400

    Update Skills component

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -26,8 +26,17 @@ function Skills(){
                 url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
                 name="React.js" />
             <SkillCard 
+                url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" 
+                name="Node.js" />
+            <SkillCard 
+                url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+                name="Express.js" />
+            <SkillCard 
                 url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg"
                 name="Firebase" />
+            <SkillCard 
+                url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+                name="MongoDB" />
             <SkillCard 
                 url="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
                 name="Git" />
