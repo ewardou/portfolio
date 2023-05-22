2a3935d23febdaedac2bae4fd98e2ff9cdab040d
Eduardo Sánchez <eduardosr1599@gmail.com>
2023-05-21 22:25:26 -0400

    Update projects section and add tags to its cards

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -2,8 +2,9 @@ import React from "react";
 import "../style/ProjectCard.css";
 import xmlIcon from "../images/xml.svg";
 import externalLinkIcon from "../images/open-in-new.svg";
+import Tags from "./Tags";
 
-function ProjectCard({title, description, minImage,image, demo, source}) {
+function ProjectCard({title, description, minImage,image, demo, source, tags}) {
     return (
         <div className="card">
             <picture>
@@ -12,6 +13,7 @@ function ProjectCard({title, description, minImage,image, demo, source}) {
             </picture>
             <h2>{title}</h2>
             <p>{description}</p>
+            <Tags tags={tags}/>
             <div>
                 <a target="_blank" href={demo} rel="noreferrer">
                         Demo <img src={externalLinkIcon} alt="" />
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "../style/Projects.css";
 import ProjectCard from "./ProjectCard";
-import calculatorImg from "../images/calculator.png"
-import calculatorImgWebp from "../images/calculator.webp";
 import shoppingImg from "../images/shopping.png";
 import shoppingImgWebp from "../images/shopping.webp";
 import waldoImg from "../images/waldo.png";
@@ -13,12 +11,12 @@ import tictactoeImg from "../images/tictactoe.png";
 import tictactoeImgWebp from "../images/tictactoe.webp";
 import battleshipImg from "../images/battleship.png";
 import battleshipImgWebp from "../images/battleship.webp";
-import todoImg from "../images/todo.png";
-import todoImgWebp from "../images/todo.webp";
-import memoryImg from "../images/memory.png";
-import memoryImgWebp from "../images/memory.webp";
 import libraryImg from "../images/library.png";
 import libraryImgWebp from "../images/library.webp";
+import blogImg from "../images/blog.png";
+import blogImgWebp from "../images/blog.webp";
+import membersImg from "../images/members.png";
+import membersImgWebp from "../images/members.webp";
 
 function Projects (){
     return (
@@ -30,58 +28,61 @@ function Projects (){
                     minImage={shoppingImgWebp}
                     image={shoppingImg}
                     demo="https://ewardou.github.io/shopping-cart/"
-                    source="https://github.com/ewardou/shopping-cart"/>
+                    source="https://github.com/ewardou/shopping-cart"
+                    tags={["React.js", "HTML", "CSS"]}/>
                 <ProjectCard title="Where's Waldo"
                     description="The app was made with React.js and Firebase. The goal is to find the targeted video game characters in the shortest time possible. It uses Firebase's firestore and cloud storage services to get the needed assets and information."
                     minImage={waldoImgWebp}
                     image={waldoImg}
                     demo="https://ewardou.github.io/wheres-waldo/"
-                    source="https://github.com/ewardou/wheres-waldo"/>
+                    source="https://github.com/ewardou/wheres-waldo"
+                    tags={["React.js", "Firebase", "HTML", "CSS"]}/>
                 
                 <ProjectCard title="Battleship"
                     description="A Battleship game project where a user plays against the computer. The player can deploy its float as desired using drag and drop or randomly with a button."
                     minImage={battleshipImgWebp}
                     image={battleshipImg}
                     demo="https://ewardou.github.io/battleship/"
-                    source="https://github.com/ewardou/battleship"/>
+                    source="https://github.com/ewardou/battleship"
+                    tags={["Javascript", "HTML", "CSS"]}/>
                 
-                <ProjectCard title="Memory card"
-                    description="This app was made with React.js. After every selection the cards shuffle. The goal is to maximize the score by not selecting the same card again. The game shows the current and best score."
-                    minImage={memoryImgWebp}
-                    image={memoryImg}
-                    demo="https://ewardou.github.io/memory-card/"
-                    source="https://github.com/ewardou/memory-card"/>
+                <ProjectCard title="Blog API"
+                    description="Personal blog app made of two client-side applications that fetch the information, in JSON format, from an API made with Express. A Mongo database stores all the blog data. Authentication with JWT is implemented for admin."
+                    minImage={blogImgWebp}
+                    image={blogImg}
+                    demo="https://ewardou.github.io/blog-front-end/"
+                    source="https://github.com/ewardou/blog-api"
+                    tags={["React.js", "Express.js", "MongoDB", "HTML", "CSS"]}/>
                 <ProjectCard title="Tic tac toe"
                     description="It is the classic game of tic tac toe. The user has the option of playing against another player locally or against the CPU."
                     minImage={tictactoeImgWebp}
                     image={tictactoeImg}
                     demo="https://ewardou.github.io/tic-tac-toe/"
-                    source="https://github.com/ewardou/tic-tac-toe"/>
-                <ProjectCard title="Todo list"
-                    description="This app is used to make lists of pending tasks. Tasks can be highlighted as relevant and you can set a deadline for them. It uses local storage to store the data for future visits."
-                    minImage={todoImgWebp}
-                    image={todoImg}
-                    demo="https://ewardou.github.io/todo-list/"
-                    source="https://github.com/ewardou/todo-list"/>
+                    source="https://github.com/ewardou/tic-tac-toe"
+                    tags={["Javascript", "HTML", "CSS"]}/>
                 <ProjectCard title="Weather app"
                     description="This app shows the current weather of a city. It uses OpenWeatherMap API to fetch the weather data. The app changes the background according to different weather conditions."
                     minImage={weatherImgWebp}
                     image={weatherImg}
                     demo="https://ewardou.github.io/weather-app/"
-                    source="https://github.com/ewardou/weather-app"/>
+                    source="https://github.com/ewardou/weather-app"
+                    tags={["Javascript", "HTML", "CSS"]}/>
                 <ProjectCard title="Library"
                     description="This application works as a personal library where the user can keep a record of planned to read / already read books. Firebase was implemented into the project to manage the books' data."
                     minImage={libraryImgWebp}
                     image={libraryImg}
                     demo="https://ewardou.github.io/library/"
-                    source="https://github.com/ewardou/library"/>
-                <ProjectCard title="Calculator"
-                    description="It is a simple Javascript calculator which can handle basic operations. It has keyboard support."
-                    minImage={calculatorImgWebp}
-                    image={calculatorImg}
-                    demo="https://ewardou.github.io/calculator/"
-                    source="https://github.com/ewardou/calculator"/>
+                    source="https://github.com/ewardou/library"
+                    tags={["Javascript", "HTML", "Firebase","CSS"]}/>
+                <ProjectCard title="Members only"
+                    description="A members' site where users can sign up and write messages, where each user has different abilities and permissions given they provide the passcode ('mango'). The app uses Passport.js with local strategy for auth."
+                    minImage={membersImgWebp}
+                    image={membersImg}
+                    demo="https://members-only-nze1.onrender.com/"
+                    source="https://github.com/ewardou/members-only"
+                    tags={["Express.js", "EJS", "MongoDB", "CSS"]}/>
             </div>
+            <a href="https://github.com/ewardou?tab=repositories" target="_blank" rel="noreferrer">More projects</a>
         </section>
     )
 }
